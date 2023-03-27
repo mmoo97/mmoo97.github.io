fbbc75bfb75bfc67729f32a79fcc2902a0c69e91
Mitchell Moore <mmoo97@uab.edu>
2023-03-26 23:48:09 -0500

    Add animation and fix bug to page reference.

--- portfolio/src/lib/menu.svelte
+++ portfolio/src/lib/menu.svelte
@@ -5,25 +5,25 @@
         anchorCorner="BOTTOM_LEFT"
     >
       <List>
-        <Item on:SMUI:action={() => (clicked = 'Home')}>
-            <IconButton href="#Home" on:click={() => {}} class="material-icons" disabled>home</IconButton>
+        <Item href="#Home" on:SMUI:action={scrollIntoView}>
+            <IconButton class="material-icons" disabled>home</IconButton>
             <Text>Home</Text>
         </Item>
-        <Item on:SMUI:action={() => (clicked = 'Projects')}>
-            <IconButton href="#Projects" on:click={() => {}} class="material-icons" disabled>code</IconButton>
+        <Item href="#Projects" on:SMUI:action={scrollIntoView}>
+            <IconButton class="material-icons" disabled>code</IconButton>
             <Text>Projects</Text>
         </Item>
-        <Item on:SMUI:action={() => (clicked = 'Tools')}>
-            <IconButton href="#Tools" on:click={() => {}} class="material-icons" disabled>build</IconButton>
+        <Item href="#Tools" on:SMUI:action={scrollIntoView}>
+            <IconButton class="material-icons" disabled>build</IconButton>
             <Text>Tools</Text>
         </Item>
-        <Item on:SMUI:action={() => (clicked = 'Education')}>
-            <IconButton href="#Education" on:click={() => {}} class="material-icons" disabled>school</IconButton>
+        <Item href="#Education" on:SMUI:action={scrollIntoView}>
+            <IconButton class="material-icons" disabled>school</IconButton>
             <Text>Education</Text>
         </Item>
         <Separator/>
-        <Item on:SMUI:action={() => (clicked = 'Contact')}>
-            <IconButton href="#Contact" on:click={() => {}} class="material-icons" disabled>contacts</IconButton>
+        <Item href="#Contact" on:SMUI:action={scrollIntoView} on:click={scrollIntoView}>
+            <IconButton class="material-icons" disabled>contacts</IconButton>
             <Text>Contact</Text>
         </Item>
       </List>
@@ -35,9 +35,15 @@
   <script lang="ts">
     import Menu from '@smui/menu';
     import List, { Item, Separator, Text } from '@smui/list';
-    import Button, { Label } from '@smui/button';
     import IconButton from '@smui/icon-button';
    
     let menu: Menu;
-    let clicked = 'nothing yet';
+
+    function scrollIntoView({ target }) {
+		const el = document.querySelector(target.getAttribute('href'));
+		if (!el) return;
+    el.scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
   </script>
